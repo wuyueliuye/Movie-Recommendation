386e288507d0221319e3a1098cbfc54851c4d08b
Guangting <47954276+wuyueliuye@users.noreply.github.com>
2021-01-12 22:06:44 -0600

    Add files via upload

---
+++ init.R
@@ -0,0 +1,17 @@
+# init.R
+#
+# install packages if not already installed
+#
+
+my_packages = c("tidyverse", "dplyr", 'lubridate','tibble', 'stringr',
+                'stringi', 'tidytext', 'superml', 'reshape2', 'caret',
+                'recommenderlab', 'shiny', 'shinythemes', 'DT')
+
+
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p)
+  }
+}
+
+invisible(sapply(my_packages, install_if_missing))
